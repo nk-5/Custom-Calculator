441c58f36b6c8d5d618c2fc09dc3bbebc4c53dc6
nkws <keigo_nakagawa@voyagegroup.com>
2017-02-16 22:36:47 +0900

    プロジェクトの作成

---
+++ CustomCalculator/CustomCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCalculator
+//
+//  Created by 中川 慶悟 on 2017/02/16.
+//  Copyright © 2017年 Keigo Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
