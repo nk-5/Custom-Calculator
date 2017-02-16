9d77a85ed00b8737465742733e78059801b88a52
nkws <keigo_nakagawa@voyagegroup.com>
2017-02-17 01:57:40 +0900

    formatの追加, plus moduleの追加

---
+++ CustomCalculator/modules/ArithmeticOperation.swift
@@ -0,0 +1,15 @@
+//
+//  ArithmeticOperation.swift
+//  CustomCalculator
+//
+//  Created by 中川 慶悟 on 2017/02/17.
+//  Copyright © 2017年 Keigo Nakagawa. All rights reserved.
+//
+
+import Foundation
+
+class ArithmeticOperation {
+    public func plus(a: Float, b: Float) -> Float {
+        return a + b
+    }
+}
--- CustomCalculatorTests/CustomCalculatorTests.swift
+++ CustomCalculatorTests/CustomCalculatorTests.swift
@@ -10,27 +10,30 @@ import XCTest
 @testable import CustomCalculator
 
 class CustomCalculatorTests: XCTestCase {
-    
+
+    let ao = ArithmeticOperation.init()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testPlus() {
+        XCTAssertEqual(2, ao.plus(a: 1, b: 1))
+        XCTAssertEqual(3, ao.plus(a: 1.5, b: 1.5))
+        XCTAssertEqual(0, ao.plus(a: 0, b: 0))
+        XCTAssertNotEqual(2, ao.plus(a: 1.5, b: 1))
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testMinus() {
     }
-    
+
+    func testMultiply() {
+    }
+
+    func testDivide() {
+    }
 }
